073d83fea4542895f0646176825a3a353675bf80
Sebastien Just <sebastien.just@seij.net>
2025-05-13 08:50:27 +0200

    bump version v0.1.1 -> v0.1.2

--- containup/cli.py
+++ containup/cli.py
@@ -6,7 +6,7 @@ from typing import List, Optional, cast
 logger = logging.getLogger(__name__)
 
 # Version is managed by bumpver. Do not touch
-_version = "v0.1.1"
+_version = "v0.1.2"
 
 
 class Config:
