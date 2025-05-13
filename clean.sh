a7081614b7facf9d2118fdb6ea288e6a8b990f35
Sebastien Just <sebastien.just@seij.net>
2025-05-13 16:53:41 +0200

    Clean script because too much noise

---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo "==> Cleanup files like __pycache__..."
+find . -type d -name "__pycache__" -exec rm -rf {} +
+find . -name "*.pyc" -delete
+rm -rf dist/ build/ *.egg-info .ruff_cache/ .pytest_cache/
+rm -rf .ruff_cache
