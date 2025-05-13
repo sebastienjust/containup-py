6da8065b36c592e7266b599d95add0e2c92e3af1
Sebastien Just <sebastien.just@seij.net>
2025-05-13 21:03:27 +0200

    Upgrade release script since main branch had been blocked (#12)

--- release.sh
+++ release.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 set -e
 
+echo "==> Create temp branch for release..."
+git checkout -b release-temp
+
 echo "==> Bump version number"
 bumpver update --patch
 
-echo "==> Remove old builds..."
-rm -rf dist/ build/ *.egg-info
+echo "==> Gets new version number"
+version=$(bumpver show | grep "Current Version" | awk '{print $NF}')
+release_branch="release/$version"
 
-echo "==> Formatat and lint..."
+echo "==> Rename temporary branch : $release_branch"
+git branch -m "$release_branch"
+
+echo "==> Remove old builds..."
+rm -rf dist/ build/ ./*.egg-info
+
+echo "==> Format and lint..."
 black .
 ruff check .
 pyright
@@ -22,9 +32,10 @@ echo "==> Chack with twine..."
 twine check dist/*
 
 echo "==> Tag Git and push if successful..."
-version=$(bumpver show | grep "Current Version" | awk '{print $NF}')
 git tag "$version" -m "Release $version"
+git push origin "$release_branch"
 git push origin "$version"
+
 
 echo "==> Ready to publish. If successful Prêt à publier. Utilise la commande suivante si tout est OK :"
 echo "twine upload --repository testpypi dist/*"
