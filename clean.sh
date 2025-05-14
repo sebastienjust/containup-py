bf3f74bc63ffdfdcd24819ba03728958782a225d
Sebastien Just <sebastien.just@seij.net>
2025-05-14 12:33:07 +0200

    Added sphinx builds in clean script

--- clean.sh
+++ clean.sh
@@ -4,3 +4,4 @@ find . -type d -name "__pycache__" -exec rm -rf {} +
 find . -name "*.pyc" -delete
 rm -rf dist/ build/ *.egg-info .ruff_cache/ .pytest_cache/
 rm -rf .ruff_cache
+make -C docs clean-docs
